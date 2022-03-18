d2e28fbb51c7d21221ba38679a6493f5c9cc8187
Fabricio Maruta <fabricio.maruta@gmail.com>
2022-03-17 21:58:25 -0300

    Fix base script

--- sp_script.sh
+++ sp_script.sh
@@ -3,7 +3,8 @@
 Q="$@"
 echo $Q
 SPTFY_URI=$( \
-             python3 spotipy-test.py $Q
+             python /usr/local/bin/spotipy-test.py $Q
          )
+
 echo $SPTFY_URI
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.OpenUri string:$SPTFY_URI
