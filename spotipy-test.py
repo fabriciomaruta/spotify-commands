0fadc3c1b917a4b14aed1cac0591ea1b84c8e29f
Fabricio Maruta <fabricio.maruta@gmail.com>
2022-03-17 21:58:42 -0300

    Add env vars to hide spotipy creds

--- spotipy-test.py
+++ spotipy-test.py
@@ -1,9 +1,13 @@
 import spotipy
 import sys
+import os
 from spotipy.oauth2 import SpotifyClientCredentials
 
+client_id = os.getenv('CLIENT_ID','')
+secret = os.getenv('SECRET','')
+
 # use spotify credentials
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='', client_secret=''))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=secret))
 arg=sys.argv
 arg.pop(0)
 
